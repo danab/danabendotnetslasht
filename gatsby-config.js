eefe70840bdfa651e5ce3f6485d9253cf327588c
Dana Bennett <danabennett10@gmail.com>
2018-10-07 22:08:06 -0400

    simple offline support (maybe?)

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Dana Bennett',
+        short_name: 'Dana Bennett',
+        start_url: '/t',
+        background_color: '#fff',
+        theme_color: '#006db3',
+        display: 'minimal-ui',
+        icon: 'src/icon.png',
+      },
+    },
+    {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography',
