fabdcc35f163ba3b79dd001f88b18b5e98f50bd3
Dana Bennett <danabennett10@gmail.com>
2018-10-07 20:28:03 -0400

    Revert, graphql being mysterious

--- src/layouts/project-post.js
+++ src/layouts/project-post.js
@@ -1,8 +1,10 @@
-import React, { Fragment } from 'react'
-import { graphql, StaticQuery } from 'gatsby'
+import React from 'react'
+import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 
-const pageQuery = graphql`
+import Layout from '../components/layout'
+
+export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
@@ -14,22 +16,16 @@ const pageQuery = graphql`
     }
   }
 `
-
-export default () => (
-  <StaticQuery
-    query={pageQuery}
-    render={data => {
-      const { markdownRemark: post } = data // data.markdownRemark holds our post data
-      return (
-        <Fragment>
-          <Helmet title={`Dana Bennett - ${post.frontmatter.title}`} />
-          <h1>{post.frontmatter.title}</h1>
-          <div
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: post.html }}
-          />
-        </Fragment>
-      )
-    }}
-  />
-)
+export default function Template({ data, location }) {
+  const { markdownRemark: post } = data
+  return (
+    <Layout location={location}>
+      <Helmet title={`Dana Bennett - ${post.frontmatter.title}`} />
+      <h1>{post.frontmatter.title}</h1>
+      <div
+        className="blog-post-content"
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
+    </Layout>
+  )
+}
