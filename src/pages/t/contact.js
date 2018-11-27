fee50ea93e42f85088e5eeeb00b55117d06341d6
Dana Bennett <danabennett10@gmail.com>
2018-11-27 07:30:43 -0500

    Edit copy

--- src/pages/t/contact.js
+++ src/pages/t/contact.js
@@ -14,21 +14,6 @@ const Contact = ({ location }) => (
           danabennett10@gmail.com.
         </p>
       </div>
-      <h2>Code</h2>
-      <div>
-        <ul>
-          <li style={{ marginLeft: '1em' }}>
-            <a href="https://github.com/danab">GitHub</a> - My day to day work
-            is in private repositories, but there are a few interesting public
-            things to see here.
-          </li>
-
-          <li style={{ marginLeft: '1em' }}>
-            <a href="https://codepen.io/danab">CodePen</a> - Occasionally quick
-            projects wind up here.
-          </li>
-        </ul>
-      </div>
     </div>
   </Layout>
 )
--- src/pages/t/index.js
+++ src/pages/t/index.js
@@ -7,12 +7,18 @@ const IndexPage = ({ location }) => (
       <h2> Hi! I'm Dana! </h2>
       <div>
         <p>
-          I'm the lead and sole developer at Call2Test, a provider of web and
-          mobile applications for drug courts. As the only developer, I must be
-          comfortable with the entire web development stack. I have learned to
-          work with our clients to determine how to add the right features to
-          our software and to give reasonable estimates of the time necessary to
-          create such features.
+          I am a full stack web developer creating quality web and mobile
+          applications. I create dynamic experiences while focusing on user
+          experience, performance, and reliability.
+        </p>
+        <p>
+          I was previously the lead developer at{' '}
+          <a href="https://call2test.com">Call2test</a>, a provider of web and
+          mobile applications for drug courts. Working as the sole developer I
+          managed the front-end, back-end, deployment, and hosting. I learned to
+          work with our clients and sales team to determine how to add the right
+          features to our software and to give reasonable estimates of the time
+          necessary to create such features.
         </p>
         <p>
           I was born and have lived most of my life in Maine, but currently
@@ -20,11 +26,21 @@ const IndexPage = ({ location }) => (
           like to spend time outdoors, which often includes hiking, mountain
           biking, snowboarding, and ultimate frisbee.
         </p>
-        <p>
-          I'm a full stack web developer creating quality web and mobile
-          applications. I create dynamic experiences while focusing on user
-          experience, performance, and reliability.
-        </p>
+      </div>
+      <h3>Code</h3>
+      <div>
+        <ul>
+          <li style={{ marginLeft: '1em' }}>
+            <a href="https://github.com/danab">GitHub</a> - My day to day work
+            is in private repositories, but there are a few interesting public
+            things to see here.
+          </li>
+
+          <li style={{ marginLeft: '1em' }}>
+            <a href="https://codepen.io/danab">CodePen</a> - Occasionally quick
+            projects wind up here.
+          </li>
+        </ul>
       </div>
       <h3>Skills</h3>
       <div>
@@ -36,7 +52,7 @@ const IndexPage = ({ location }) => (
               <li>React Native</li>
               <li>Node/Express</li>
               <li>D3</li>
-              <li>ES6</li>
+              <li>ES6/ES2017/ES2018</li>
             </ul>
           </li>
           <li>
@@ -45,8 +61,10 @@ const IndexPage = ({ location }) => (
               <li>CodeIgniter</li>
             </ul>
           </li>
+          <li>MySQL</li>
           <li>HTML</li>
           <li>CSS</li>
+          <li>AWS</li>
         </ul>
       </div>
     </div>
